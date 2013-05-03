efa2f928b5fc33e7b6e916b29a7bf93d392808c2
Geoff Schorkopf <gschorkopf@gmail.com>
2013-05-03 16:19:32 -0600

    New implementation for wins, losses, and matches

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,5 +1,10 @@
 class Player < ActiveRecord::Base
-  attr_accessible :score, :name
+  attr_accessible :name, :score
 
-  has_many :matches, foreign_key: [:winner_id, :loser_id]
+  has_many :wins, class_name: 'Match', foreign_key: 'winner_id'
+  has_many :losses, class_name: 'Match', foreign_key: 'loser_id'
+
+  def matches
+    wins + losses
+  end
 end
